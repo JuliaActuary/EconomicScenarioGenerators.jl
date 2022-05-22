731c4e99115c0386def87706686a8b4698ec4096
Alec Loudenback <alecloudenback@gmail.com>
2022-05-22 15:47:21 -0500

    WIP first cut

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# ESG
+# ESG.jl
 
 [![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://alecloudenback.github.io/ESG.jl/stable)
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://alecloudenback.github.io/ESG.jl/dev)
 [![Build Status](https://github.com/alecloudenback/ESG.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/alecloudenback/ESG.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/alecloudenback/ESG.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/alecloudenback/ESG.jl)
+
+Help wanted!
+
+This package is very early in its development cycle.
+
+Interested in developing economic scenario generators in Julia? Consider contributing to this package. Open an issue, create a pull request, or discuss on the Julia Zulip's #actuary channel.
--- src/ESG.jl
+++ src/ESG.jl
@@ -1,5 +1,53 @@
 module ESG
 
-# Write your package code here.
+abstract type InterestRateModel end
 
+abstract type ShortRateModel <: InterestRateModel end
+
+
+"""
+Via Wikipedia: https://en.wikipedia.org/wiki/Vasicek_model
+"""
+struct Vasicek <: ShortRateModel
+    a # 0.136
+    b # .0168
+    σ # .0119
+end
+
+"""
+outputtype defines what the iterator's type output is for each element
+"""
+outputtype(::Type{Vasicek}) = Float64
+
+struct ScenarioGenerator{T}
+    timestep
+    length
+    model::T
+end
+
+function Base.length(sg::ScenarioGenerator{T}) where {T<:InterestRateModel}
+    return sg.timestep * sg.length
+end
+function Base.iterate(sg::ScenarioGenerator{T}) where {T<:InterestRateModel}
+    return (1,1)
+end
+
+function Base.iterate(sg::ScenarioGenerator{T},state) where {T<:InterestRateModel}
+    if state >= sg.timestep * sg.length
+        return nothing
+    else
+        return (state+1, state+1)
+    end
+end
+
+Base.eltype(::Type{ScenarioGenerator{T}}) where {T<:InterestRateModel} = outputtype(T)
+
+struct Squares
+    count::Int
+end
+
+#iteration example
+Base.iterate(S::Squares, state=1) = state > S.count ? nothing : (state*state, state+1)
+Base.eltype(::Type{Squares}) = Int 
+Base.length(S::Squares) = S.count
 end
