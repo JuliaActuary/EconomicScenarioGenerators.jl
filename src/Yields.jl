849714bb6ca6e7fafb3a91d4fbd0ae9069bc077b
alecloudenback <alecloudenback@users.noreply.github.com>
2022-08-01 23:25:41 -0500

    cleanup

--- src/Yields.jl
+++ src/Yields.jl
@@ -9,12 +9,8 @@ function __disc_rate_to_fwd(rate,time)
 end
 
 function __zeros_times(sg::ScenarioGenerator{N,T,R}) where {N,T<:InterestRateModel,R}
-    # times = sg.timestep:sg.timestep:(sg.endtime+sg.timestep)
     times = sg.timestep:sg.timestep:(sg.endtime+sg.timestep)
     # compute the accumulated discount factor (ZCB price)
-    # floor at zero in order to avoid having a negative discount factor 
-    # return 1 .- cumsum(Yields.rate.(sg) .* sg.timestep)
-    # rates = __disc_rate_to_fwd.(1 .- cumsum(Yields.rate.(sg) .* sg.timestep),times)
     zeros = cumsum(sg .* sg.timestep) ./ times
 
     return zeros, times
