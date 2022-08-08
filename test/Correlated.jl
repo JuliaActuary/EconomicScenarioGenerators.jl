c9f08f1200e9e4b4133078274dcdf7c587d83925
alecloudenback <alecloudenback@users.noreply.github.com>
2022-08-07 22:24:58 -0500

    add IR test

--- test/Correlated.jl
+++ test/Correlated.jl
@@ -17,4 +17,28 @@
 
     ρ = cor(hcat(ratio.(x)...))
     @test ρ[1,2] ≈ 0.9 atol = 0.03
+
+    @testset "Interest" begin
+        @testset "HullWhite" begin
+            rates =[0.01, 0.01, 0.03, 0.05, 0.07, 0.16, 0.35, 0.92, 1.40, 1.74, 2.31, 2.41] ./ 100
+            mats = [1/12, 2/12, 3/12, 6/12, 1, 2, 3, 5, 7, 10, 20, 30]
+            c = Yields.CMT(rates,mats)
+
+            m = HullWhite(.1,.001,c)
+
+            s = ScenarioGenerator(
+                .1,                              # timestep
+                30.,                             # projection horizon
+                m
+            )
+
+            g = GaussianCopula([1. 0.9; 0.9 1.])
+            c = Correlated(ss,g,StableRNG(1))
+
+            x = collect(c)
+
+            ρ = cor(hcat(ratio.(x)...))
+            @test ρ[1,2] ≈ 0.9 atol = 0.03
+        end
+    end
 end
